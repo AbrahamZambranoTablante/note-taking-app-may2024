0938005e66a489aca07c169de23e4c8c8a6d5095
AbrahamZambranoTablante <vbrahamzt@gmail.com>
2024-05-04 12:09:05 -0400

    adding notelist component, all required files header, nav, notes

---
+++ src/NoteList/note-list.jsx
@@ -0,0 +1,12 @@
+
+
+export default function Home ({notes}) {
+    return (
+        <div>
+            <h1>Notes</h1>
+            <div>
+                {/* {notes.map(note => )} */}
+            </div>
+        </div>
+    )
+}
