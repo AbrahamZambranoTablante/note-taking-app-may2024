c1e987d1c54b274dfff2ccfe7ce5f0466d4da7bc
AbrahamZambranoTablante <vbrahamzt@gmail.com>
2024-05-05 12:00:08 -0400

    adding search bar on the home/notelist page

--- src/NoteList/note-list.jsx
+++ src/NoteList/note-list.jsx
@@ -6,16 +6,42 @@ import "./note-list.css"
 export default function Home () {
     const [allNotes, setAllNotes] = useState([]);
 
+    const [searchNote, setSearchNote] = useState("");
+
+    const [notes, setNotes] = useState([]);
 
     useEffect(() => {
-        getAllNotes().then(res => setAllNotes(res))
+        getAllNotes().then(res => {
+            setAllNotes(res);
+            setNotes(res);
+        })
     },[])
+
+
+    function handleTextChange(event) {
+        const search = event.target.value;
+        const result = search.length ? filterNotes(search, allNotes) : allNotes;
+        setSearchNote(search);
+        setNotes(result);
+    }
+
+    function filterNotes(search, notes) {
+        return notes.filter(note => note.title.toLowerCase().match(search.toLowerCase()) || note.category.toLowerCase().match(search.toLowerCase()));
+    }
 
     return (
         <>
-            <h1 className="notes-title">Notes</h1>
-            <div className="note-list">
-                {allNotes.map(note => <Note key={note.id} note={note}/>)}
+            <div className="note-list-container">
+                <h1 className="notes-title">Notes</h1>
+                <input 
+                type="text"
+                value={searchNote}
+                placeholder="Search Notes"
+                onChange={handleTextChange}
+                />
+                <div className="note-list">
+                    {notes.map(note => <Note key={note.id} note={note}/>)}
+                </div>
             </div>
         </>
     )
